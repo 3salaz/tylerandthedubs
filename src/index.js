3d7d4784c5ddcd0d0821b56c5e8e77c53524d351
3salaz <3salaz.dev@gmail.com>
2022-05-11 14:22:40 -0700

    added firebase app to webpack config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import getWarriorData from './getData';
-import app from './firebase';
+import { initializeApp } from "firebase/app";
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6MdFrc3dMZd_gocFnT6_c06kDuYmhOd0",
+  authDomain: "nativefanatics.firebaseapp.com",
+  projectId: "nativefanatics",
+  storageBucket: "nativefanatics.appspot.com",
+  messagingSenderId: "606356471271",
+  appId: "1:606356471271:web:6c42fd5afa872529295142"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 console.log(getWarriorData());
 console.log(app);
-console.log('Hello World');
